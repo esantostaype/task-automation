b5bb219b43c7221caea1ff2eac2b0f110feb7874
Erick <esantostaype@gmail.com>
2025-08-04 10:29:15 -0500

    login

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,19 +1,18 @@
-import { Providers } from '../providers'
 import { CreateTaskForm, GlobalModal, Header } from '@/components'
 import { GlobalConfirmation } from '@/components/Confirmation'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Providers>
-      <main className='flex'>
-        <section className='flex-1 h-dvh overflow-y-auto flex flex-col'>
-          <Header />
-          {children}
-        </section>
-        <CreateTaskForm />
-      </main>
-      <GlobalModal />
-      <GlobalConfirmation />
-    </Providers>
+    <>
+    <main className='flex'>
+      <section className='flex-1 h-dvh overflow-y-auto flex flex-col'>
+        <Header />
+        {children}
+      </section>
+      <CreateTaskForm />
+    </main>
+    <GlobalModal />
+    <GlobalConfirmation />
+    </>
   )
 }
